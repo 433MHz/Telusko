1434ec3ee05879c38807f4d555f1d7a0e96b6d03
user <user@DESKTOP-HNKK2FJ>
2020-10-24 11:37:06 +0200

    First time commit

---
+++ LearningTelusko/src/MainClass.java
@@ -0,0 +1,18 @@
+import java.util.TreeMap;
+
+public class MainClass {
+	
+	public static void main(String args[]){
+	
+		TreeMap<Integer, String> h = new TreeMap<>();
+		
+		
+		h.put(1, "Ja");
+		h.put(5, "Tom");
+		h.put(4, "Ktoœ");
+		
+		System.out.println(h.get(4));
+
+	}
+}
+
