3301d833c12d5eee954b743b173848a6c17105f4
user <user@DESKTOP-HNKK2FJ>
2020-10-24 11:52:55 +0200

    changed branch and ArrayList inner

--- LearningTelusko/src/MainClass.java
+++ LearningTelusko/src/MainClass.java
@@ -1,18 +1,10 @@
-import java.util.ArrayList;
 
 public class MainClass {
 	
 	public static void main(String args[]){
 	
-	ArrayList<String> a = new ArrayList<>();
-
-	
-	a.add("Tomek");
-	a.add("Krystian");
-	
-	for(String x : a) {
-		System.out.println(x);
-	}
+		System.out.println("ala ma kota");
+		
 	}
 }
 
